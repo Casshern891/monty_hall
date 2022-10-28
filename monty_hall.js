d057f9959de07fd79583944564ae02b83b9cf31a
Richard Lovell <cassh3rn@talktalk.net>
2022-10-28 19:24:15 +0100

    first commit

---
+++ monty_hall.js
@@ -0,0 +1,44 @@
+let car = Math.floor(Math.random() * 3) + 1;
+let choice = Math.floor(Math.random() * 3) + 1;
+let revealed = 0;
+let result = "Goat!"
+if (choice === 1) {
+    if (car === 1) {
+        revealed = 2;
+        choice = 3;
+    } else if (car === 2) {
+        revealed = 3;
+        choice = 2;
+    } else if (car === 3) {
+        revealed = 2;
+        choice = 3;
+    }
+} else if (choice === 2) {
+    if (car === 1) {
+        revealed = 3;
+        choice = 1;
+    } else if (car === 2) {
+        revealed = 1;
+        choice = 3;
+    } else if (car === 3) {
+        revealed = 1;
+        choice = 3;
+    }
+} else if (choice === 3) {
+    if (car === 1) {
+        revealed = 2;
+        choice = 1;
+    } else if (car === 2) {
+        revealed = 1;
+        choice = 2;
+    } else if (car === 3) {
+        revealed = 1;
+        choice = 2;
+    }
+}
+
+if (choice === car) {
+    result = "Car!"
+}
+
+console.log(result);
